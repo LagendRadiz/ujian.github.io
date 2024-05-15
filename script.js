27bfac5412a08e69c5932aa1e748ecb4c0201f02
radiz <pintar054@gmail.com>
2024-05-15 23:03:22 +0700

    menambahkan fitur simpan name dan nilai di form shet

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// ----------------
+// ----Kirim data user-----
+// ----------------
+
+const login = document.getElementById("login");
+const name_input = document.getElementById("name_input");
+const big_container = document.getElementById("big_container");
+const nama = document.getElementById("get_name");
+
+let wadah_name_user = name_input.value;
+
+function lanjut() {
+  if (name_input.value === "") {
+    alert("Tolong masukan nama lengkap anda ");
+  } else {
+    login.style.display = "none";
+    big_container.style.display = "block";
+
+    let wadah_name_user = name_input.value;
+    nama.value = wadah_name_user;
+    console.log(wadah_name_user);
+  }
+}
+
+// ----------------
+// ----Section-----
+// ----------------
+
 const humberger = document.getElementById("humberger");
 const list = document.getElementById("list");
 
@@ -19,6 +47,9 @@ const form_hasil = document.querySelector(".c-hasil");
 // fungsi haram
 function selesai() {
   check_radio();
+  const nilai = document.getElementById("nilai");
+  nilai.value = scor;
+  console.log(jawaban_user);
 }
 
 function check_radio() {
@@ -48,7 +79,6 @@ function check_radio() {
     checked_radio10 != null
   ) {
     calculate();
-    result.textContent = "Selamat nilai kamu: " + scor;
     form_hasil.style.display = "flex";
   } else {
     alert("Ada yang belum di jawab");
@@ -81,18 +111,25 @@ function calculate() {
   jawaban_user.push(parseInt(checked_radio10.getAttribute("data-id")));
 
   for (let i = 0; i < jawaban_benar.length; i++) {
-    if (jawaban_user[i] == jawaban_benar[i]) {
-      scor += 100;
+    if (jawaban_user[i] === jawaban_benar[i]) {
+      scor += 10;
     }
   }
 }
 
-form_hasil.addEventListener("click", function () {
-  form_hasil.style.display = "none";
-
+function submit() {
   let checked_radio1 = document.querySelector('input[name="option1"]:checked');
   let checked_radio2 = document.querySelector('input[name="option2"]:checked');
   let checked_radio3 = document.querySelector('input[name="option3"]:checked');
+  let checked_radio4 = document.querySelector('input[name="option4"]:checked');
+  let checked_radio5 = document.querySelector('input[name="option5"]:checked');
+  let checked_radio6 = document.querySelector('input[name="option6"]:checked');
+  let checked_radio7 = document.querySelector('input[name="option7"]:checked');
+  let checked_radio8 = document.querySelector('input[name="option8"]:checked');
+  let checked_radio9 = document.querySelector('input[name="option9"]:checked');
+  let checked_radio10 = document.querySelector(
+    'input[name="option10"]:checked'
+  );
 
   jawaban_user = [];
   scor = 0;
@@ -100,7 +137,16 @@ form_hasil.addEventListener("click", function () {
   checked_radio1.checked = false;
   checked_radio2.checked = false;
   checked_radio3.checked = false;
-});
+  checked_radio4.checked = false;
+  checked_radio5.checked = false;
+  checked_radio6.checked = false;
+  checked_radio7.checked = false;
+  checked_radio8.checked = false;
+  checked_radio9.checked = false;
+  checked_radio10.checked = false;
+
+  form_hasil.style.display = "none";
+}
 
 const c_manipulator = document.getElementById("img-manipulator");
 const mode_cupu = document.getElementById("mode_cupu");
